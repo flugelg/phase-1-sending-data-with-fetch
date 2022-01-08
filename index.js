879197a60d3779d83e4c4ea4e874795e38383571
Gary Flugel <flugelg@gmail.com>
2022-01-08 04:07:43 -0500

    commit tree 2822e03d726b2dda2ac5b14dad467969104b3561

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            email,
+        })
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            let p = document.createElement("p");
+            document.body.appendChild(p);
+            p.innerHTML = object.id;
+            console.log(object);
+        })
+        .catch(function (error) {
+            alert("Error");
+            let p = document.createElement("p");
+            document.body.appendChild(p);
+            p.innerHTML = error.message;
+        });
+}
